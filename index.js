b88b4cbd02b6455a04953c5b8151a5c9f6e296df
apluszcz <ap.temporary.rb@gmail.com>
2018-06-13 16:31:11 +0200

    models | add Painting model

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const hapi = require('hapi');
 const mongoose = require('mongoose');
+const Painting = require('./models/Painting');
 
 
 const server = hapi.server({
@@ -12,6 +13,27 @@ const init = async () => {
     method: 'GET',
     path: '/',
     handler: (request, reply) => `<h1>My modern API</h1>`
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/api/v1/paintings',
+    handler: (request, reply) => Painting.find()
+  });
+
+  server.route({
+    method: 'POST',
+    path: '/api/v1/paintings',
+    handler: (request, reply) => {
+      const { name, url, techniques } = request.payload;
+      const painting = new Painting({
+        name,
+        url,
+        techniques
+      });
+
+      return painting.save();
+    }
   });
 
   await server.start();
